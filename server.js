414821da6c0c029c8d5210e0f630c22f3eb1e979
abstract4713 <mallabhinav@hotmail.com>
2015-10-28 23:17:07 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+
+server.register(require('inert'), function (err) {
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+
+/*
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});*/
+
+
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+
+server.stop(function () {
+    console.log('Server stopped at:', server.info.uri);
+});
+
+
+
+
